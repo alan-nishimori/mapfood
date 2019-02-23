ff745f1661ec9044cc506bb3a46306c5604b4181
alan-nishimori <alan.nishimori@gmail.com>
2019-02-23 17:17:57 -0300

    primeiro commit

---
+++ src/main/java/mapfood/controller/TestController.java
@@ -0,0 +1,16 @@
+package mapfood.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+    @GetMapping
+    public HttpStatus test() {
+        return HttpStatus.OK;
+    }
+}
