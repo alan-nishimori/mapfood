79a76d12907cb30ec6a0f578cb09b297f2ed1fc1
mario7cr <mario.7cr@gmail.com>
2019-03-10 13:28:01 -0300

    bug fix dos testes

--- src/test/java/mapfood/service/MapfoodServiceImplTest.java
+++ src/test/java/mapfood/service/MapfoodServiceImplTest.java
@@ -9,17 +9,20 @@ import mapfood.repository.OrderRepository;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.geo.Point;
 import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+@RunWith(SpringRunner.class)
 @ExtendWith(MockitoExtension.class)
 public class MapfoodServiceImplTest {
 
